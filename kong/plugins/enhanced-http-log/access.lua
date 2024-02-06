57597d0c2e5b7c84bb2c52d3e4c905ca46b64ca6
Prakoso Santoso <prakoso.santoso@numtech.com>
2024-02-06 16:26:05 +0800

    Change to kong.ctx.plugin to share request body

--- kong/plugins/enhanced-http-log/access.lua
+++ kong/plugins/enhanced-http-log/access.lua
@@ -3,7 +3,7 @@ local kong = kong
 
 function _M.execute()
   local body = kong.request.get_body("application/json")
-  kong.ctx.shared.request_body = body or {}
+  kong.ctx.plugin.request_body = body or {}
 end
 
 return _M
