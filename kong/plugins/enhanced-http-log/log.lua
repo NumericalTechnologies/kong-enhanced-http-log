71ed3a4279fb37052c43601d2d9326db345d63cc
Prakoso Santoso <prakoso.santoso@numtech.com>
2024-02-06 16:13:19 +0800

    Add missing dependencies

--- kong/plugins/enhanced-http-log/log.lua
+++ kong/plugins/enhanced-http-log/log.lua
@@ -3,7 +3,6 @@ local cjson = require "cjson"
 local url = require "socket.url"
 local http = require "resty.http"
 local sandbox = require "kong.tools.sandbox".sandbox
-local kong_meta = require "kong.meta"
 
 
 local kong = kong
