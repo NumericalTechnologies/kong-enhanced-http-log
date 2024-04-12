70d054e7250a939fea07b716a3a9be53f058b290
Prakoso Santoso <prakoso.santoso@numtech.com>
2024-04-12 15:52:13 +0800

    Log request headers and body

--- kong/plugins/enhanced-http-log/access.lua
+++ kong/plugins/enhanced-http-log/access.lua
@@ -4,6 +4,9 @@ local kong = kong
 function _M.execute()
   kong.ctx.plugin.request_headers = kong.request.get_headers()
   kong.ctx.plugin.request_body = kong.request.get_body("application/json")
+
+  kong.log.info(kong.ctx.plugin.request_headers)
+  kong.log.info(kong.ctx.plugin.request_body)
 end
 
 return _M
