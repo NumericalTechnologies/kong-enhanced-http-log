200893e4a2fb927db9ad5eed2f8edadeb87333e9
Prakoso Santoso <prakoso.santoso@numtech.com>
2024-04-12 15:03:44 +0800

    Force body to get as application/json

--- kong/plugins/enhanced-http-log/access.lua
+++ kong/plugins/enhanced-http-log/access.lua
@@ -3,7 +3,7 @@ local kong = kong
 
 function _M.execute()
   kong.ctx.plugin.request_headers = kong.request.get_headers() or {}
-  kong.ctx.plugin.request_body = kong.request.get_body() or {}
+  kong.ctx.plugin.request_body = kong.request.get_body("application/json") or {}
 end
 
 return _M
